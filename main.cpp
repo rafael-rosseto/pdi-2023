b767f905ddc53c95f518ab4449ab767bbf261d9d
Rafael Rosseto <rafael-rosseto@unesp.br>
2023-03-27 08:06:40 -0300

    using 1d vector

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ using namespace pgm;
 
 int main(int argc, char *argv[]) {
     image input, output;
-    loadPgm(input, "lena256.pgm");
-    normalizar(input, output);
-    savePgm(output, "out.pgm");
-    thread th1(system, "visualizar lena256.pgm");
-    thread th2(system, "visualizar out.pgm");
+    carregarPgm(input, "imagens\\lena.pgm");
+    mediana(input, output, 2);
+    salvarPgm(output, "temp.pgm");
+    thread th1(system, "visualizar imagens\\lena.pgm");
+    thread th2(system, "visualizar temp.pgm");
     th1.join();
     th2.join();
+    system("del temp*");
     return 0;
 }
--- pgm.hpp
+++ pgm.hpp
@@ -5,9 +5,12 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 #define pi 3.14159265359
+#define img(i, j) (img.data[(i)*img.w + (j)])
+#define out(i, j) (out.data[(i)*out.w + (j)])
 
 using namespace std;
 
@@ -16,13 +19,10 @@ namespace pgm {
 struct image {
     string type;
     int w, h, bits;
-    vector<vector<uint8_t>> data;
+    vector<uint8_t> data;
 };
 
-vector<vector<float>> dctdata;
-int dct_width, dct_height;
-
-void eatComment(ifstream &f) {
+void lerComentario(ifstream &f) {
     char linebuf[1024];
     char ppp;
     while (ppp = f.peek(), ppp == '\n' || ppp == '\r')
@@ -31,33 +31,38 @@ void eatComment(ifstream &f) {
         f.getline(linebuf, 1023);
 }
 
-void loadPgm(image &img, const string &name) {
-    ifstream f(name.c_str(), ios::in);
+void carregarPgm(image &img, const string &name) {
+    ifstream f(name.c_str(), ios::binary);
     if (f.fail()) {
         cout << "Erro ao abrir o arquivo " << name << endl;
         return;
     }
-    eatComment(f);
+    lerComentario(f);
     f >> img.type;
-    eatComment(f);
+    lerComentario(f);
     f >> img.w;
-    eatComment(f);
+    lerComentario(f);
     f >> img.h;
-    eatComment(f);
+    lerComentario(f);
     f >> img.bits;
-    img.data.resize(img.w, vector<uint8_t>(img.h));
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            int v;
-            f >> v;
-            img.data[i][j] = v;
+    img.data.resize(img.w * img.h);
+    if (img.type == "P5") {
+        f.get();
+        f.read(reinterpret_cast<char *>(img.data.data()), img.data.size());
+    } else if (img.type == "P2") {
+        for (int i = 0; i < img.w; i++) {
+            for (int j = 0; j < img.h; j++) {
+                int v;
+                f >> v;
+                img(i, j) = v;
+            }
         }
     }
     f.close();
 }
 
-void savePgm(image &img, const string &name) {
-    ofstream f(name.c_str(), ios::out);
+void salvarPgm(image &img, const string &name) {
+    ofstream f(name.c_str(), ios::binary);
     if (f.fail()) {
         cout << "Erro ao abrir o arquivo " << name << endl;
         return;
@@ -65,26 +70,29 @@ void savePgm(image &img, const string &name) {
     f << img.type << endl
       << img.w << ' ' << img.h << endl
       << img.bits << endl;
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            f << +img.data[i][j] << endl;
+    if (img.type == "P5") {
+        f.write((char *)&img.data[0], img.data.size());
+    } else if (img.type == "P2") {
+        for (int i = 0; i < img.w; i++) {
+            for (int j = 0; j < img.h; j++) {
+                f << +img(i, j) << endl;
+            }
         }
     }
     f.close();
 }
 
 void printHeader(image img) {
-    cout << "PGM type:   " << img.type << endl;
-    cout << "PGM width:  " << img.w << endl;
-    cout << "PGM height: " << img.h << endl;
-    cout << "PGM bits:   " << img.bits << endl;
+    cout << img.type << endl
+         << img.w << ' ' << img.h << endl
+         << img.bits << endl;
 }
 
 void printData(image img) {
     for (int i = 0; i < img.w; i++) {
         for (int j = 0; j < img.h; j++) {
             cout << setw(3);
-            cout << +img.data[i][j] << ' ';
+            cout << +img(i, j) << ' ';
         }
         cout << endl;
     }
@@ -96,13 +104,13 @@ void cloneHeader(image img, image &out) {
     out.w = img.w;
     out.h = img.h;
     out.data.clear();
-    out.data.resize(img.w, vector<uint8_t>(img.h));
+    out.data.resize(img.w * img.h);
 }
 
 void cloneData(image img, image &out) {
     for (int i = 0; i < img.w; i++) {
         for (int j = 0; j < img.h; j++) {
-            out.data[i][j] = img.data[i][j];
+            out(i, j) = img(i, j);
         }
     }
 }
@@ -111,7 +119,7 @@ void inverter(image img, image &out) {
     cloneHeader(img, out);
     for (int i = 0; i < img.w; i++) {
         for (int j = 0; j < img.h; j++) {
-            out.data[i][j] = 255 - img.data[i][j];
+            out(i, j) = 255 - img(i, j);
         }
     }
 }
@@ -120,7 +128,7 @@ void rotacionar(image img, image &out) {
     cloneHeader(img, out);
     for (int i = 0; i < img.w; i++) {
         for (int j = 0; j < img.h; j++) {
-            out.data[i][j] = img.data[j][i];
+            out(i, j) = img(j, i);
         }
     }
 }
@@ -129,15 +137,42 @@ void brilho(image img, image &out, float num) {
     cloneHeader(img, out);
     for (int i = 0; i < img.w; i++) {
         for (int j = 0; j < img.h; j++) {
-            int aux = img.data[i][j] * num;
+            int aux = img(i, j) * num;
             if (aux > 255)
-                out.data[i][j] = 255;
+                out(i, j) = 255;
             else if (aux < 0)
-                out.data[i][j] = 0;
+                out(i, j) = 0;
             else
-                out.data[i][j] = aux;
+                out(i, j) = aux;
         }
     }
+}
+
+void normalizar(image img, image &out) {
+    cloneHeader(img, out);
+    int max = 0, min = 255;
+    for (int i = 0; i < img.w; i++) {
+        for (int j = 0; j < img.h; j++) {
+            if (img(i, j) > max) max = img(i, j);
+            if (img(i, j) < min) min = img(i, j);
+        }
+    }
+    for (int i = 0; i < img.w; i++) {
+        for (int j = 0; j < img.h; j++) {
+            out(i, j) = (img(i, j) - min) * 255 / (max - min);
+        }
+    }
+}
+
+void tons(image img, image &out, int nivel) {
+    int aux = 255 / nivel;
+    cloneHeader(img, out);
+    for (int i = 0; i < img.w; i++) {
+        for (int j = 0; j < img.h; j++) {
+            out(i, j) = ((uint8_t)img(i, j) / aux) * aux;
+        }
+    }
+    normalizar(out, out);
 }
 
 void media(image img, image &out, int range) {
@@ -150,11 +185,11 @@ void media(image img, image &out, int range) {
                 for (int jj = j - range; jj <= j + range; jj++) {
                     if (ii < 0 || jj < 0 || ii >= img.h || jj >= img.w)
                         continue;
-                    pixel += img.data[ii][jj];
+                    pixel += img(ii, jj);
                     count++;
                 }
             }
-            out.data[i][j] = pixel * (1.0f / count);
+            out(i, j) = pixel * (1.0f / count);
         }
     }
 }
@@ -169,142 +204,12 @@ void mediana(image img, image &out, int range) {
                 for (int jj = j - range; jj <= j + range; jj++) {
                     if (ii < 0 || jj < 0 || ii >= img.w || jj >= img.h)
                         continue;
-                    pixel.push_back(img.data[ii][jj]);
+                    pixel.push_back(img(ii, jj));
                     count++;
                 }
             }
             sort(pixel.begin(), pixel.end());
-            out.data[i][j] = pixel.at(count / 2 + 1);
-        }
-    }
-}
-
-void normalizar(image img, image &out) {
-    cloneHeader(img, out);
-    int max = 0, min = 255;
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            if (img.data[i][j] > max) max = img.data[i][j];
-            if (img.data[i][j] < min) min = img.data[i][j];
-        }
-    }
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            out.data[i][j] = (img.data[i][j] - min) * 255 / (max - min);
-        }
-    }
-}
-
-void dctTransform(image img) {
-    dct_width = img.w;
-    dct_height = img.h;
-    dctdata.resize(dct_width, vector<float>(dct_height));
-    float ci, cj, dct1, sum;
-    for (int i = 0; i < dct_width; i++) {
-        for (int j = 0; j < dct_height; j++) {
-            if (i == 0)
-                ci = 1 / sqrt(dct_width);
-            else
-                ci = sqrt(2) / sqrt(dct_width);
-            if (j == 0)
-                cj = 1 / sqrt(dct_height);
-            else
-                cj = sqrt(2) / sqrt(dct_height);
-            sum = 0;
-            for (int k = 0; k < dct_width; k++) {
-                for (int l = 0; l < dct_height; l++) {
-                    dct1 = img.data[k][l] *
-                           cos((2 * k + 1) * i * pi / (2 * dct_width)) *
-                           cos((2 * l + 1) * j * pi / (2 * dct_height));
-                    sum = sum + dct1;
-                }
-            }
-            dctdata[i][j] = ci * cj * sum;
-        }
-    }
-}
-
-void idctTransform(image &out) {
-    float ci, cj, dct1, sum;
-    for (int i = 0; i < dct_width; i++) {
-        for (int j = 0; j < dct_height; j++) {
-            if (i == 0)
-                ci = 1 / sqrt(dct_width);
-            else
-                ci = sqrt(2) / sqrt(dct_width);
-            if (j == 0)
-                cj = 1 / sqrt(dct_height);
-            else
-                cj = sqrt(2) / sqrt(dct_height);
-            sum = 0;
-            for (int k = 0; k < dct_width; k++) {
-                for (int l = 0; l < dct_height; l++) {
-                    dct1 = dctdata[k][l] * ci * cj *
-                           cos((2 * k + 1) * i * pi / (2 * dct_width)) *
-                           cos((2 * l + 1) * j * pi / (2 * dct_height));
-                    sum = sum + dct1;
-                }
-            }
-            out.data[i][j] = sum;
-        }
-    }
-}
-
-void normalizarDct(image &out) {
-    float max = numeric_limits<float>::min();
-    float min = numeric_limits<float>::max();
-    for (int i = 0; i < dct_width; i++) {
-        for (int j = 0; j < dct_height; j++) {
-            if (dctdata[i][j] > max) max = dctdata[i][j];
-            if (dctdata[i][j] < min) min = dctdata[i][j];
-        }
-    }
-    for (int i = 0; i < dct_width; i++) {
-        for (int j = 0; j < dct_height; j++) {
-            out.data[i][j] = (dctdata[i][j] - min) * 255 / (max - min);
-        }
-    }
-}
-
-void laplaciano(image img, image &out) {
-    cloneHeader(img, out);
-    int masc[9] = {1, 1, 1,
-                   1, -8, 1,
-                   1, 1, 1};
-    int pos_masc = 0;
-    int cont_masc = 0;
-    for (int i = 1; i < img.w - 1; i++) {
-        for (int j = 1; j < img.h - 1; j++) {
-            cont_masc = 0;
-            for (int ii = -1; ii <= 1; ii++) {
-                for (int jj = -1; jj <= 1; jj++, cont_masc++) {
-                    pos_masc += img.data[i + ii][j + jj] * masc[cont_masc];
-                }
-            }
-            pos_masc /= 9;
-            out.data[i][j] = pos_masc;
-        }
-    }
-}
-
-void laplacGaussiano(image img, image &out) {
-    cloneHeader(img, out);
-    int masc[25] = {0, 0, 1, 0, 0,
-                    0, 1, 2, 1, 0,
-                    1, 2, -16, 2, 1,
-                    0, 1, 2, 1, 0,
-                    0, 0, 1, 0, 0};
-    int pos_masc = 0, cont_masc = 0;
-    for (int i = 2; i < img.w - 2; i++) {
-        for (int j = 2; j < img.h - 2; j++) {
-            cont_masc = 0;
-            for (int ii = -2; ii <= 2; ii++) {
-                for (int jj = -2; jj <= 2; jj++, cont_masc++) {
-                    pos_masc += img.data[i + ii][j + jj] * masc[cont_masc];
-                }
-            }
-            pos_masc /= 25;
-            out.data[i][j] = pos_masc;
+            out(i, j) = pixel.at(count / 2 + 1);
         }
     }
 }
