c9d89a9482d8635439bb63757b5ead1fb9a42b33
Rafael Rosseto <rafaelbassirosseto@gmail.com>
2023-04-02 19:17:42 -0300

    histograma rgb

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <thread>
 
-#include "pgm.hpp"
+#include "ppm.hpp"
 
 using namespace std;
-using namespace pgm;
+using namespace ppm;
 
 int main(int argc, char *argv[]) {
-    image input, output;
-    carregarPgm(input, "imagens\\lenna.pgm");
-    histograma(input, output);
-    salvarPgm(output, "temp.ppm");
-    thread th1(system, "visualizar imagens\\lenna.pgm");
-    thread th2(system, "visualizar temp.ppm");
+    imagem input, histR, histG, histB;
+    carregarImagem(input, "imagens\\lenna.ppm");
+    histograma(input, histR, histG, histB);
+    salvarImagem(histR, "temp-histR.ppm");
+    salvarImagem(histG, "temp-histG.ppm");
+    salvarImagem(histB, "temp-histB.ppm");
+    thread th1(system, "visualizar imagens\\lenna.ppm");
+    thread th2(system, "visualizar temp-histR.ppm");
+    thread th3(system, "visualizar temp-histG.ppm");
+    thread th4(system, "visualizar temp-histB.ppm");
     th1.join();
     th2.join();
+    th3.join();
+    th4.join();
     system("del temp*");
     return 0;
 }
--- ppm.hpp
+++ ppm.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <algorithm>
-#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
 #define pi 3.14159265359
+#define src(i, j) (src.data[(i)*src.w + (j)])
+#define dst(i, j) (dst.data[(i)*dst.w + (j)])
 
 using namespace std;
 
@@ -16,13 +17,13 @@ struct RGB {
     uint8_t r, g, b;
 };
 
-struct image {
+struct imagem {
     string type;
     int w, h, bits;
-    vector<vector<RGB>> data;
+    vector<RGB> data;
 };
 
-void eatComment(ifstream &f) {
+void lerComentario(ifstream &f) {
     char linebuf[1024];
     char ppp;
     while (ppp = f.peek(), ppp == '\n' || ppp == '\r')
@@ -31,145 +32,258 @@ void eatComment(ifstream &f) {
         f.getline(linebuf, 1023);
 }
 
-void loadPpm(image &img, const string &name) {
+void carregarImagem(imagem &src, const string &name) {
     ifstream f(name.c_str(), ios::binary);
     if (f.fail()) {
         cout << "Could not open file: " << name << endl;
         return;
     }
-    eatComment(f);
-    f >> img.type;
-    eatComment(f);
-    f >> img.w;
-    eatComment(f);
-    f >> img.h;
-    eatComment(f);
-    f >> img.bits;
-    img.data.resize(img.w, vector<RGB>(img.h));
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
+    lerComentario(f);
+    f >> src.type;
+    lerComentario(f);
+    f >> src.w;
+    lerComentario(f);
+    f >> src.h;
+    lerComentario(f);
+    int bits = 0;
+    f >> bits;
+    src.data.resize(src.w * src.h);
+    if (src.type == "P6") {
+        f.get();
+        f.read(reinterpret_cast<char *>(src.data.data()), src.data.size() * 3);
+    } else if (src.type == "P3") {
+        for (int i = 0; i < src.data.size(); i++) {
             int v;
             f >> v;
-            img.data[i][j].r = v;
+            src.data[i].r = v;
             f >> v;
-            img.data[i][j].g = v;
+            src.data[i].g = v;
             f >> v;
-            img.data[i][j].b = v;
+            src.data[i].b = v;
         }
     }
     f.close();
 }
 
-void savePpm(image &img, const string &name) {
-    ofstream f(name.c_str(), ios::out);
+void salvarImagem(imagem &src, const string &name) {
+    ofstream f(name.c_str(), ios::binary);
     if (f.fail()) {
         cout << "Erro ao abrir o arquivo " << name << endl;
         return;
     }
-    f << img.type << endl
-      << img.w << ' ' << img.h << endl
-      << img.bits << endl;
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            f << +img.data[i][j].r << ' ';
-            f << +img.data[i][j].g << ' ';
-            f << +img.data[i][j].b << endl;
+    f << src.type << endl
+      << src.w << ' ' << src.h << endl
+      << src.bits << endl;
+    if (src.type == "P6") {
+        f.write((char *)&src.data[0], src.data.size() * 3);
+    } else if (src.type == "P3") {
+        for (int i = 0; i < src.h; i++) {
+            for (int j = 0; j < src.w; j++) {
+                f << +src(i, j).r << ' ';
+                f << +src(i, j).g << ' ';
+                f << +src(i, j).b << endl;
+            }
         }
-    }
-    f.close();
-}
-
-void printHeader(image img) {
-    cout << "PGM type:   " << img.type << endl;
-    cout << "PGM width:  " << img.w << endl;
-    cout << "PGM height: " << img.h << endl;
-    cout << "PGM bits:   " << img.bits << endl;
-}
-
-void printData(image img) {
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            cout << +img.data[i][j].r << ' ';
-            cout << +img.data[i][j].g << ' ';
-            cout << +img.data[i][j].b << endl;
-        }
+        f.close();
     }
 }
 
-void cloneHeader(image img, image &out) {
-    out.type = img.type;
-    out.bits = img.bits;
-    out.w = img.w;
-    out.h = img.h;
-    out.data.clear();
-    out.data.resize(img.w, vector<RGB>(img.h));
+void printHeader(imagem src) {
+    cout << src.type << endl
+         << src.w << ' ' << src.h << endl
+         << src.bits << endl;
 }
 
-void cloneData(image img, image &out) {
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            out.data[i][j].r = img.data[i][j].r;
-            out.data[i][j].g = img.data[i][j].g;
-            out.data[i][j].b = img.data[i][j].b;
+void printData(imagem src) {
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            cout << +src(i, j).r << ' ';
+            cout << +src(i, j).g << ' ';
+            cout << +src(i, j).b << endl;
         }
     }
 }
 
-void invert(image img, image &out) {
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
-            out.data[i][j].r = 255 - img.data[i][j].r;
-            out.data[i][j].g = 255 - img.data[i][j].g;
-            out.data[i][j].b = 255 - img.data[i][j].b;
+void setHeader(imagem &src, string type, int w, int h, int bits) {
+    src.type = type;
+    src.w = w;
+    src.h = h;
+    src.bits = bits;
+    src.data.clear();
+    src.data.resize(w * h);
+}
+
+void clonarCabecalho(imagem src, imagem &dst) {
+    dst.type = src.type;
+    dst.bits = src.bits;
+    dst.w = src.w;
+    dst.h = src.h;
+    dst.data.clear();
+    dst.data.resize(src.w * src.h);
+}
+
+void clonarDados(imagem src, imagem &dst) {
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src(i, j);
         }
     }
 }
 
-void media(image img, image &out, int range) {
-    cloneHeader(img, out);
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
+void inverter(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j).r = 255 - src(i, j).r;
+            dst(i, j).g = 255 - src(i, j).g;
+            dst(i, j).b = 255 - src(i, j).b;
+        }
+    }
+}
+
+void transposta(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src(j, i);
+        }
+    }
+}
+
+void flipHorizontal(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src(i, (src.h - j - 1));
+        }
+    }
+}
+
+void flipVertical(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src((src.w - i - 1), j);
+        }
+    }
+}
+
+void girar90(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src((src.h - j - 1), i);
+        }
+    }
+}
+
+void girarM90(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src(j, (src.w - i - 1));
+        }
+    }
+}
+
+void girar180(imagem src, imagem &dst) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            dst(i, j) = src((src.w - i - 1), (src.h - j - 1));
+        }
+    }
+}
+
+void histograma(imagem src, imagem &dst1, imagem &dst2, imagem &dst3) {
+    vector<int> R(256, 0), G(256, 0), B(256, 0);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
+            R[src(i, j).r]++;
+            G[src(i, j).g]++;
+            B[src(i, j).b]++;
+        }
+    }
+    int max = 0;
+    for (int i = 0; i < 256; i++) {
+        if (R[i] > max) max = R[i];
+        if (G[i] > max) max = G[i];
+        if (B[i] > max) max = B[i];
+    }
+    for (int i = 0; i < 256; i++) {
+        R[i] = R[i] * 255 / max;
+        G[i] = G[i] * 255 / max;
+        B[i] = B[i] * 255 / max;
+    }
+    setHeader(dst1, "P6", 256, 256, 255);
+    setHeader(dst2, "P6", 256, 256, 255);
+    setHeader(dst3, "P6", 256, 256, 255);
+    for (int i = 0; i < 256; i++) {
+        for (int j = 0; j < 256; j++) {
+            if (i < R[j])
+                dst1.data[i * dst1.w + j] = {255, 0, 0};
+            else
+                dst1.data[i * dst1.w + j] = {0, 0, 0};
+            if (i < G[j])
+                dst2.data[i * dst2.w + j] = {0, 255, 0};
+            else
+                dst2.data[i * dst2.w + j] = {0, 0, 0};
+            if (i < B[j])
+                dst3.data[i * dst3.w + j] = {0, 0, 255};
+            else
+                dst3.data[i * dst3.w + j] = {0, 0, 0};
+        }
+    }
+    flipVertical(dst1, dst1);
+    flipVertical(dst2, dst2);
+    flipVertical(dst3, dst3);
+}
+
+void media(imagem src, imagem &dst, int range) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
             int R = 0, G = 0, B = 0;
             int count = 0;
             for (int ii = i - range; ii <= i + range; ii++) {
                 for (int jj = j - range; jj <= j + range; jj++) {
-                    if (ii < 0 || jj < 0 || ii >= img.h || jj >= img.w)
+                    if (ii < 0 || jj < 0 || ii >= src.h || jj >= src.w)
                         continue;
-                    R += img.data[ii][jj].r;
-                    G += img.data[ii][jj].g;
-                    B += img.data[ii][jj].b;
+                    R += src(ii, jj).r;
+                    G += src(ii, jj).g;
+                    B += src(ii, jj).b;
                     count++;
                 }
             }
-            out.data[i][j].r = R * (1.0f / count);
-            out.data[i][j].g = G * (1.0f / count);
-            out.data[i][j].b = B * (1.0f / count);
+            dst(i, j).r = R * (1.0f / count);
+            dst(i, j).g = G * (1.0f / count);
+            dst(i, j).b = B * (1.0f / count);
         }
     }
 }
 
-void mediana(image img, image &out, int range) {
-    cloneHeader(img, out);
-    for (int i = 0; i < img.w; i++) {
-        for (int j = 0; j < img.h; j++) {
+void mediana(imagem src, imagem &dst, int range) {
+    clonarCabecalho(src, dst);
+    for (int i = 0; i < src.h; i++) {
+        for (int j = 0; j < src.w; j++) {
             vector<int> R, G, B;
             int count = 0;
             for (int ii = i - range; ii <= i + range; ii++) {
                 for (int jj = j - range; jj <= j + range; jj++) {
-                    if (ii < 0 || jj < 0 || ii >= img.w || jj >= img.h)
+                    if (ii < 0 || jj < 0 || ii >= src.h || jj >= src.w)
                         continue;
-                    R.push_back(img.data[ii][jj].r);
-                    G.push_back(img.data[ii][jj].g);
-                    B.push_back(img.data[ii][jj].b);
+                    R.push_back(src(ii, jj).r);
+                    G.push_back(src(ii, jj).g);
+                    B.push_back(src(ii, jj).b);
                     count++;
                 }
             }
             sort(R.begin(), R.end());
-            out.data[i][j].r = R.at(count / 2 + 1);
+            dst(i, j).r = R.at(count / 2 + 1);
             sort(G.begin(), G.end());
-            out.data[i][j].g = G.at(count / 2 + 1);
+            dst(i, j).g = G.at(count / 2 + 1);
             sort(B.begin(), B.end());
-            out.data[i][j].b = B.at(count / 2 + 1);
+            dst(i, j).b = B.at(count / 2 + 1);
         }
     }
 }
